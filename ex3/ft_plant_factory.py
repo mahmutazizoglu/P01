82a637387a8d06f74d1330774a222fbb7fc47d29
Mahmut Sevket Azizoglu <maazizog@3-A-13.42heilbronn.de>
2026-08-02 23:09:27 +0200

    ex0,1,2

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -1,21 +1,30 @@
 class Plant():
-    def __init__(self, name, height, age, growth_rate):
+    def __init__(self,
+                 name: str,
+                 height: float,
+                 age: int,
+                 growth_rate: float
+                 ) -> None:
         self.name = name
         self.height = float(height)
         self.age = age
         self.growth_rate = growth_rate
+
+    def show(self) -> None:
         print(f"Created: {self.name}: {self.height}cm, {self.age} days old")
 
-    def show(self):
-        print(f"Created: {self.name}: {self.height}cm, {self.age} days old")
-
-    def grow(self):
+    def grow(self) -> None:
         self.height += self.growth_rate
 
 
 print("=== Plant Factory Output ===")
 rose = Plant("Rose", 25.0, 30, 0.2)
+rose.show()
 oak = Plant("Oak", 200.0, 365, 0.2)
+oak.show()
 cactus = Plant("Cactus", 5.0, 90, 0.2)
+cactus.show()
 sunflower = Plant("Sunflower", 80.0, 45, 0.2)
+sunflower.show()
 fern = Plant("Fern", 15.0, 120, 0.2)
+fern.show()
